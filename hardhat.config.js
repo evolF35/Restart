07545538def6b6515594700a9c10f278a33e38a6
evolF35 <maccyair@gmail.com>
2022-12-12 09:02:18 +0400

    is it working

---
+++ hardhat.config.js
@@ -0,0 +1,35 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
+require("@nomiclabs/hardhat-ethers");
+
+require("@nomiclabs/hardhat-etherscan");
+
+
+
+const GoerliUrl =
+process.env.ALCHEMY_API_KEY ?
+   `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}` :
+   process.env.OPTIMISM_GOERLI_URL
+
+const optimismGoerliUrl =
+   process.env.ALCHEMY_API_KEY ?
+      `https://opt-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}` :
+      process.env.OPTIMISM_GOERLI_URL
+
+
+module.exports = {
+  solidity: "0.8.9",
+  etherscan:{
+    apiKey: `${process.env.ETHERSCAN_API_KEY}`
+  },
+  networks: {
+    "optimism-goerli": {
+       url: optimismGoerliUrl,
+       accounts: { mnemonic: process.env.MNEMONIC }
+    },
+    "goerli": {
+      url: GoerliUrl,
+      accounts: { mnemonic: process.env.MNEMONIC }
+   }
+  }
+};
